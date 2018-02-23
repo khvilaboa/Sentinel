c74f259a859e6ceae5beafed1200c68df191f07f
khvilaboa <khvilaboa@esei.uvigo.es>
2018-02-24 00:13:09 +0100

    Django template
    Basic Django project. It includes an example structure and some models
    and views examples.

---
+++ app/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'app'
+urlpatterns = [
+    # ex: /app/
+    url(r'^$', views.index, name='index'),
+    # ex: /app/5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /app/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /app/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+]
